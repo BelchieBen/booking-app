f653e1cf9718b8ad7e456705036d679674bfdeb9
benjamin.belcher <benjamin.belcher@ideagen.com>
2022-04-19 13:27:10 +0100

    Finished behavour on editable table, still need to implement state  store for the table

--- frontend/src/components/EditableTable.jsx
+++ frontend/src/components/EditableTable.jsx
@@ -8,10 +8,100 @@ import TopTips from '../utils/images/content-images/TopTips.svg'
 import Video from '../utils/images/content-images/Video.svg'
 
 import * as React from 'react';
-import {Box, Stack} from '@mui/material';
-import { DataGrid } from '@mui/x-data-grid';
+import {Box, Stack, Select, MenuItem, FormControl, Link} from '@mui/material';
+import { DataGrid, useGridApiContext  } from '@mui/x-data-grid';
+import Dropdown from './Dropdown';
 
-export default function EditableTable(props) {
+// Test Data
+const contentTypes = [
+  {
+      item: {
+        contentType:"Article",
+        icon: Article
+      }
+  },
+  {
+      item: {
+        contentType:"E-Book",
+        icon: EBook
+      }
+  },
+  {
+      item: {
+       contentType:"E-Learning",
+       icon: ELearning
+      }
+  },
+  {
+    item: {
+      contentType:"Face to Face",
+      icon: FaceToFace
+    }
+  },
+  {
+    item: {
+      contentType:"Online",
+      icon: Online
+    }
+  },
+  {
+    item: {
+      contentType:"Podcast",
+      icon: Podcast
+    }
+  },
+  {
+    item: {
+      contentType:"Top Tips",
+      icon: TopTips
+    }
+  },
+  {
+    item: {
+      contentType:"Video",
+      icon: Video
+    }
+  }
+]
+
+function renderContent(params) {
+  return(
+    <Stack direction="row" sx={{alignItems:"center"}}>
+      <Box component="img" sx={{height:30, width:30, marginRight:2}} src={params.value.icon} />
+      {params.value.contentType}
+    </Stack>
+  );
+}
+
+function ContentTypeEditInput(props) {
+  const { id, value, field } = props;
+  const apiRef = useGridApiContext();
+
+  const handleChange = (event, newValue) => {
+    apiRef.current.setEditCellValue({ id, field, value: newValue.props.value });
+  };
+
+  return (
+    <FormControl fullWidth>
+        <Select
+            id="content-type-select"
+            value={value}
+            size="small"
+            onChange={handleChange}>
+
+        {contentTypes.map(data => (
+            <MenuItem value={data.item}>
+              <Box component="img" sx={{height:25, width:25, marginRight:2}} src={data.item.icon} />
+              {data.item.contentType}
+            </MenuItem>
+        ))}
+
+        </Select>
+    </FormControl>
+    );
+}
+
+ export default function EditableTable(props) {
   return (
     <Box
       sx={{
@@ -20,6 +110,7 @@ export default function EditableTable(props) {
       }}
     >
       <DataGrid
+      sx={{cursor: 'pointer'}}
         rows={rows}
         columns={columns}
         isCellEditable={props.isReadyOnly}
@@ -29,38 +120,42 @@ export default function EditableTable(props) {
   );
 }
 
+const renderContentTypeEditInput = (params) => {
+  return <ContentTypeEditInput {...params} />
+}
+
 const columns = [
     { 
         field: 'content', 
         headerName: 'Content',
-        renderCell: (params) => {
-          return(
-            <Stack direction="row" sx={{alignItems:"center"}}>
-              <Box component="img" sx={{height:30, width:30, marginRight:2}} src={params.value.icon} />
-              {params.value.contentType}
-            </Stack>
-          )
-        },
-        flex:1, 
+        renderCell: renderContent,
+        renderEditCell: renderContentTypeEditInput,
+        flex:0.2, 
         editable: true,
         sortComparator: (v1, v2) => v1.contentType.localeCompare(v2.contentType)
     },
     { 
         field: 'title', 
         headerName: 'Title', 
-        flex:1, 
-        editable: true 
+        renderCell: (params) => {
+          return(
+            <Link href={params.value.href} target="_blank" rel="noopener noreferrer">{params.value.titleText}</Link>
+          )
+        },
+        flex:0.4,
+        editable: true ,
+        sortComparator: (v1, v2) => v1.titleText.localeCompare(v2.titleText)
     },
     {
         field: 'source',
         headerName: 'Source',
-        flex:1, 
+        flex:0.3, 
         editable: true,
     },
     {
         field: 'duration',
         headerName: 'Duration',
-        flex:1, 
+        flex:0.15,
         editable: true,
     },
 ];
@@ -72,7 +167,10 @@ const rows = [
       contentType:"Article",
       icon: Article
     },
-    title: "The GROW Model of Coaching and Mentoring",
+    title: {
+      titleText:"The GROW Model of Coaching and Mentoring",
+      href: "https://www.mindtools.com/pages/article/newLDR_89.htm",
+    },
     source: "Mind Tools",
     duration: "9 mins",
   },
@@ -82,7 +180,10 @@ const rows = [
       contentType:"Article",
       icon: Article
     },
-    title: "The 5 Essentials to Effective Coaching",
+    title: {
+      titleText:"The 5 Essentials to Effective Coaching E",
+      href: "https://www.entrepreneur.com/article/292877",
+    },
     source: "Entrepreneur.com",
     duration: "5 mins",
   },
@@ -92,7 +193,10 @@ const rows = [
       contentType:"Article",
       icon: Article
     },
-    title: "The Key to Effective Coaching",
+    title: {
+      titleText:"The Key to Effective Coaching",
+      href: "https://www.forbes.com/2010/04/28/coaching-talent-development-leadership-managing-ccl.html?sh=1b14b6ca38e0",
+    },
     source: "Forbes",
     duration: "7 mins",
   },
@@ -102,7 +206,10 @@ const rows = [
       contentType:"Podcast",
       icon: Podcast
     },
-    title: "Coaching for Leaders Podcast",
+    title: {
+      titleText:"Coaching for Leaders Podcast",
+      href: "https://coachingforleaders.com/",
+    },
     source: "Dave Stachowiak",
     duration: "Various",
   },
@@ -112,7 +219,10 @@ const rows = [
       contentType:"Podcast",
       icon: Podcast
     },
-    title: "Accelerating Growth through Coaching",
+    title: {
+      titleText:"Accelerating Growth through Coaching",
+      href: "https://awesomeatyourjob.com/631-accelerating-growth-through-coaching-with-andrea-wanerstrand/",
+    },
     source: "How to be Awesome at Your Job",
     duration: "42 mins",
   },
